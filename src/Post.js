2f705261a0e456e2c2bbd60c67e74eec370d14f2
BTG001 <brunokipkorir2@gmail.com>
2020-10-07 01:55:31 +0300

    Add dummy link for a photo

--- src/Post.js
+++ src/Post.js
@@ -7,8 +7,10 @@ function Post() {
       <h3>Username</h3>
       {/* header -> avatar + username */}
 
+      <img src="https://s3.amazonaws.com/pixpa.com/com/articles/1559139403-725019-modelposesjpg.png"></img>
       {/* image */}
 
+      <h4>Username: caption</h4>
       {/* username + caaption */}
     </div>
   );
