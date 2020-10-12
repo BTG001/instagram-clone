96d472ae543094613bd0328dfc3a797fde57c928
BTG001 <brunokipkorir2@gmail.com>
2020-10-12 20:42:15 +0300

    Import Avatar from material ui into the react app

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,18 @@
 /*Type rfce and hit enter - everything is filled up for us*/
 import React from "react";
 import "./Post.css";
+import Avatar from "@material-ui/core";
 
 function Post() {
   return (
     <div className="post">
+      <div className="post__header">
+      <Avatar
+        className="post__avatar"
+        alt="Bruno"
+        src="/static/images/avatar/1.jpg"/>
       <h3>Username</h3>
+      </div>
       {/* header -> avatar + username */}
 
       <img
@@ -13,10 +20,10 @@ function Post() {
         src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
         alt="React"
       ></img>
-      {/* image */}
 
-      <h4>Username: caption</h4>
-      {/* username + caaption */}
+      <h4 className="post__text">
+        <strong>bruno: </strong> Wow! Day Three of building this
+      </h4>
     </div>
   );
 }
