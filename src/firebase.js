4327facce1b26481c63e3f48f3d7d303bb795ac7
BTG001 <brunokipkorir2@gmail.com>
2020-10-13 00:57:49 +0300

    Create firebase.js file which will be responsible for all config settings

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-0n9YgfIVVHs7JmP7Csw3FxFQjdUfM-k",
+  authDomain: "instagram-clone-react-3f4e7.firebaseapp.com",
+  databaseURL: "https://instagram-clone-react-3f4e7.firebaseio.com",
+  projectId: "instagram-clone-react-3f4e7",
+  storageBucket: "instagram-clone-react-3f4e7.appspot.com",
+  messagingSenderId: "65994828055",
+  appId: "1:65994828055:web:729763bb4b6825015caf52",
+  measurementId: "G-7PR5Y8YQGD"
+};
