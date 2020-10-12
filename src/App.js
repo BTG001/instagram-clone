5e2972c07aa380b865a3dee039906847c8430e4c
BTG001 <brunokipkorir2@gmail.com>
2020-10-12 23:23:21 +0300

    Differentiate each post by using props(pass imageUrl as props)

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ function App() {
 
       <h1>Instagram Clone with React 🚀 !</h1>
 
+      <Post imageUrl="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18.08.13.png" />
       <Post />
-      {/* Posts */}
-      {/* Posts */}
+      <Post />
+
     </div>
   );
 }
